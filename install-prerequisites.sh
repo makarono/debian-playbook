13aecbbc348ce15d8c040952fb6f342a6e195937
David Burilovic <david.burilovic@aduro.hr>
2021-05-31 13:48:16 +0200

    changed vagrant test

--- install-prerequisites.sh
+++ install-prerequisites.sh
@@ -14,9 +14,9 @@ function get-ansible-roles {
 echo -n "download ansible roles"
 
 
-get-ansible-roles gantsign.atom
-get-ansible-roles tonyapuzzo.ansible-role-atom
-get-ansible-roles mhalano.visual-studio-code
+get-ansible-roles "gantsign.atom"
+get-ansible-roles "tonyapuzzo.ansible-role-atom"
+get-ansible-roles "mhalano.visual-studio-code"
 get-ansible-roles "git+https://github.com/oscpe262/ansible-role-exa.git"
 get-ansible-roles "oefenweb.virtualbox"
 get-ansible-roles "cmprescott.chrome"
